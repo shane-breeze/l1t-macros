90617e2bb4b3774c4254185ea94a939cc77c728f
Shane Breeze <s.breeze15@imperial.ac.uk>
2016-09-30 16:07:02 +0200

    Generalise the submission scripts

--- submitXvsY.sh
+++ submitXvsY.sh
@@ -2,8 +2,8 @@
 # run with:
 # $ bsub -q 8nh "sh submitTurnons.sh"
 
-cd /afs/cern.ch/work/s/sbreeze/CMSSW_8_0_9/src/
+cd $CMSSW_BASE/src
 eval `scramv1 runtime -sh`
-cd /afs/cern.ch/work/s/sbreeze/l1tClasses/
+cd -
 
 root -q -b -l "MakePlots/makeXvsY.cxx($1,$2,$3,$4)"
