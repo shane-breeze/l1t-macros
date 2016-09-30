e7a9c7077997f59c704f72d0754cde5d867f6308
Shane Breeze <s.breeze15@imperial.ac.uk>
2016-09-30 11:16:37 +0200

    Finished batch submission configurations for resolutions

--- MakePlots/makeResolutions.cxx
+++ MakePlots/makeResolutions.cxx
@@ -7,146 +7,78 @@
 #include "../Utilities/TL1DateTime.C"
 #include "../Plotting/TL1Resolution.h"
 
-std::vector<double> bins(std::string plotType);
-void SetMyStyle(int palette, double rmarg, TStyle * myStyle);
+#include "../Config/ntuple_cfg.h"
+#include "../Config/sumResolutions_cfg.h"
+
+#include "../Debug/DebugHandler.h"
+
 double FoldPhi(double phi);
 
-void makeResolutions(const int & SET, const bool & combine)
+// CHUNK = which chunk of root-files to run over
+// NFILES = numbers of root-files
+// NJOBS = number of jobs to submit
+void makeResolutions(const int & CHUNK, const int & NFILES, const int & NJOBS, const bool & COMBINE)
 {
+    // Check CHUNK < NJOBS
+    DebugHandler::ErrorCheck(CHUNK >= NJOBS, "The CHUNK number exceeds the number of jobs", __FILE__, __LINE__);
+
+    // Set ROOT style
     TStyle * myStyle(new TStyle(TDRStyle()));
     SetMyStyle(55, 0.07, myStyle);
 
-    // Basic
-    std::string sampleName = "Data";
-    std::string sampleTitle = "2016 Data";
-    std::string triggerName = "SingleMu";
-    std::string triggerTitle = "Single Muon";
-    // std::string sampleName = "HInv";
-    // std::string sampleTitle = "VBF H #rightarrow Inv";
-    // std::string triggerName = "";
-    // std::string triggerTitle = "";
-    std::string puFilename = "/afs/cern.ch/work/s/sbreeze/l1tClasses/PUWeights/20160719_Data-SingleMu-2016Bv1_VBFHinv/pu_mcReweightedToData.root";
+    // Get config objects
+    ntuple_cfg * dataset = new ntuple_cfg(GetNtuple_cfg());
+    std::map< std::string, TL1Resolution* > resolutions = sumResolutions(dataset);
 
-    // std::string run = "2016Bv1";
-    // std::string run = "";
-    std::string run = "276525";
-    std::string outDirBase = "/afs/cern.ch/work/s/sbreeze/L1TriggerStudiesOutput";
-    std::vector<std::string> puType = {"0PU12","13PU19","20PU"};
-    std::vector<int> puBins = {0,13,20,999};
-
-    std::string outDir("");
+    // Split the files into CHUNKS:
+    // 1 to n; n+1 to 2n; 2n+1 to 3n ... NJOBS*n to NFILES
+    // n = nFilesPerJob
+    // The final job is typically larger than the others (never smaller)
     std::vector<std::string> inDir;
-    // inDir.push_back("/afs/cern.ch/work/s/sbreeze/public/jets_and_sums/160511_l1t-integration-v48p2/SingleMu/Ntuples");
-    // inDir.push_back("/afs/cern.ch/work/s/sbreeze/public/jets_and_sums/160519_l1t-integration-v53p1/SingleMu_273301/Ntuples");
-    // inDir.push_back("/afs/cern.ch/work/s/sbreeze/public/jets_and_sums/160602_r273450_SingleMu_l1t-int-v53p1");
-    // inDir.push_back("/afs/cern.ch/work/s/sbreeze/public/jets_and_sums/160607_combinedRuns_SingleMu");
-    // inDir.push_back("/afs/cern.ch/work/s/sbreeze/public/jets_and_sums/160704_SingleMu2016Bv1_l1t-int-v67p0");
-    // inDir.push_back("/afs/cern.ch/work/s/sbreeze/public/jets_and_sums/160713_r276243_SingleMu_l1t-int-71p1/");
-    // inDir.push_back("/afs/cern.ch/work/s/sbreeze/public/jets_and_sums/160718_MC_VBFHinv125GeV_l1t-int-70p2");
-    if(!combine)
+    std::string outDir( dataset->outDir+"_hadd/Resolution/" );
+    if( !COMBINE )
     {
-        std::string files = "root://eoscms.cern.ch//eos/cms/store/group/dpg_trigger/comm_trigger/L1Trigger/L1Menu2016/Stage2/Collision2016-wRECO-l1t-integration-v71p1/SingleMuon/crab_Collision2016-wRECO-l1t-integration-v71p1__276525_SingleMuon/160713_153738/0000/L1Ntuple_%i.root";
-        for(int i=1+(SET*10); i<=10+(SET*10); ++i)
-            inDir.push_back(Form(files.c_str(),i));
-        // std::string outDir = outDirBase+"/"+TL1DateTime::GetDate()+"_MC_"+sampleName+"_highMET/Resolution/";
-        outDir = outDirBase+"/"+TL1DateTime::GetDate()+"_"+sampleName+"_"+"run-"+run+"_"+triggerName+Form("_highMET_SET%i/Resolution/",SET);
+        outDir = dataset->outDir;
+        if( NJOBS > 1 ) outDir += Form("_CHUNK%i",CHUNK);
+        outDir += "/Resolution/";
+
+        int nFilesPerJob( NFILES / NJOBS );
+        int finalFile( nFilesPerJob*(1+CHUNK) );
+        if( CHUNK == NJOBS-1 ) finalFile = NFILES;
+
+        for(int i=1+(CHUNK*nFilesPerJob); i<=finalFile; ++i)
+            inDir.push_back(Form(dataset->inFiles.c_str(),i));
     }
-    else
-    {
-        // std::string outDir = outDirBase+"/"+TL1DateTime::GetDate()+"_MC_"+sampleName+"_highMET/Resolution/";
-        outDir = outDirBase+"/"+TL1DateTime::GetDate()+"_"+sampleName+"_"+"run-"+run+"_"+triggerName+"_highMET_hadd/Resolution/";
-    }
-    
     TL1EventClass * event(new TL1EventClass(inDir));
-    std::vector<TL1Resolution*> resolution;
 
-    std::string baseOWdir = "/afs/cern.ch/work/s/sbreeze/L1TriggerStudiesOutput/20160729_Data_run-276525_SingleMu_highMET_hadd/Turnons/";
-
-    // caloMetBE vs l1MetBE
-    resolution.emplace_back(new TL1Resolution());
-    if(combine) resolution[0]->SetOverwriteNames(baseOWdir+"res_SingleMu_diff_caloMetBE_l1MetBE.root","res_diff_caloMetBE_vs_l1MetBE");
-    resolution[0]->SetPlotType("Energy");
-    resolution[0]->SetBins(bins("Energy"));
-    resolution[0]->SetX("caloMetBE","Offline E_{T}^{miss} BE");
-    resolution[0]->SetY("l1MetBE","L1 E_{T}^{miss} BE");
-    resolution[0]->SetOutName(triggerName+"_diff_caloMetBE_l1MetBE");
-    resolution[0]->SetAddMark("Offline E_{T}^{miss} > 40 GeV");
-
-    // caloMetHF vs l1MetBE
-    resolution.emplace_back(new TL1Resolution());
-    if(combine) resolution[1]->SetOverwriteNames(baseOWdir+"res_SingleMu_diff_caloMetHF_l1MetBE.root","res_diff_caloMetHF_vs_l1MetBE");
-    resolution[1]->SetPlotType("Energy");
-    resolution[1]->SetBins(bins("Energy"));
-    resolution[1]->SetX("caloMetHF","Offline E_{T}^{miss} HF");
-    resolution[1]->SetY("l1MetBE","L1 E_{T}^{miss} BE");
-    resolution[1]->SetOutName(triggerName+"_diff_caloMetHF_l1MetBE");
-    resolution[1]->SetAddMark("Offline E_{T}^{miss} > 40 GeV");
-
-    // caloMetHF vs l1MetHF
-    resolution.emplace_back(new TL1Resolution());
-    if(combine) resolution[2]->SetOverwriteNames(baseOWdir+"res_SingleMu_diff_caloMetHF_l1MetHF.root","res_diff_caloMetHF_vs_l1MetHF");
-    resolution[2]->SetPlotType("Energy");
-    resolution[2]->SetBins(bins("Energy"));
-    resolution[2]->SetX("caloMetHF","Offline E_{T}^{miss} HF");
-    resolution[2]->SetY("l1MetHF","L1 E_{T}^{miss} HF");
-    resolution[2]->SetOutName(triggerName+"_diff_caloMetHF_l1MetHF");
-    resolution[2]->SetAddMark("Offline E_{T}^{miss} > 40 GeV");
-
-    // caloMetPhiBE vs l1MetPhiBE
-    resolution.emplace_back(new TL1Resolution());
-    if(combine) resolution[3]->SetOverwriteNames(baseOWdir+"res_SingleMu_diff_caloMetPhiBE_l1MetPhiBE.root","res_diff_caloMetPhiBE_vs_l1MetPhiBE");
-    resolution[3]->SetPlotType("Position");
-    resolution[3]->SetBins(bins("Position"));
-    resolution[3]->SetX("caloMetPhiBE","Offline E_{T}^{miss} Phi BE");
-    resolution[3]->SetY("l1MetPhiBE","L1 E_{T}^{miss} Phi BE");
-    resolution[3]->SetOutName(triggerName+"_diff_caloMetPhiBE_l1MetPhiBE");
-    resolution[3]->SetAddMark("Offline E_{T}^{miss} > 40 GeV");
-
-    // caloMetPhiHF vs l1MetPhiBE
-    resolution.emplace_back(new TL1Resolution());
-    if(combine) resolution[4]->SetOverwriteNames(baseOWdir+"res_SingleMu_diff_caloMetPhiHF_l1MetPhiBE.root","res_diff_caloMetPhiHF_vs_l1MetPhiBE");
-    resolution[4]->SetPlotType("Position");
-    resolution[4]->SetBins(bins("Position"));
-    resolution[4]->SetX("caloMetPhiHF","Offline E_{T}^{miss} Phi HF");
-    resolution[4]->SetY("l1MetPhiBE","L1 E_{T}^{miss} Phi BE");
-    resolution[4]->SetOutName(triggerName+"_diff_caloMetPhiHF_l1MetPhiBE");
-    resolution[4]->SetAddMark("Offline E_{T}^{miss} > 40 GeV");
-
-    // caloMetPhiHF vs l1MetPhiHF
-    resolution.emplace_back(new TL1Resolution());
-    if(combine) resolution[5]->SetOverwriteNames(baseOWdir+"res_SingleMu_diff_caloMetPhiHF_l1MetPhiHF.root","res_diff_caloMetPhiHF_vs_l1MetPhiHF");
-    resolution[5]->SetPlotType("Position");
-    resolution[5]->SetBins(bins("Position"));
-    resolution[5]->SetX("caloMetPhiHF","Offline E_{T}^{miss} Phi HF");
-    resolution[5]->SetY("l1MetPhiHF","L1 E_{T}^{miss} Phi HF");
-    resolution[5]->SetOutName(triggerName+"_diff_caloMetPhiHF_l1MetPhiHF");
-    resolution[5]->SetAddMark("Offline E_{T}^{miss} > 40 GeV");
-
-    for(auto it=resolution.begin(); it!=resolution.end(); ++it)
+    // Being
+    for(auto it=resolutions.begin(); it!=resolutions.end(); ++it)
     {
-        (*it)->SetSample(sampleName,sampleTitle);
-        (*it)->SetTrigger(triggerName,triggerTitle);
-        (*it)->SetRun(run);
-        (*it)->SetOutDir(outDir);
-        (*it)->SetPuType(puType);
-        (*it)->SetPuBins(puBins);
-        (*it)->SetPuFile(puFilename);
-        if( !combine ) (*it)->InitPlots();
-        else (*it)->OverwritePlots();
+        it->second->SetSample(dataset->sampleName,dataset->sampleTitle);
+        it->second->SetTrigger(dataset->triggerName,dataset->triggerTitle);
+        it->second->SetRun(dataset->run);
+        it->second->SetOutDir(outDir);
+        it->second->SetPuType(dataset->puType);
+        it->second->SetPuBins(dataset->puBins);
+        if( dataset->sampleName != "Data" ) it->second->SetPuFile(dataset->puFilename);
+        if( !COMBINE ) it->second->InitPlots();
+        else it->second->OverwritePlots();
     }
 
+    // Loop
     unsigned NEntries(0);
-    if(!combine) NEntries = event->GetPEvent()->GetNEntries();
-    while( event->Next() && (!combine) )
+    if( !COMBINE ) NEntries = event->GetPEvent()->GetNEntries();
+    while( event->Next() && !COMBINE )
     {
-       
         unsigned position = event->GetPEvent()->GetPosition()+1;
         TL1Progress::PrintProgressBar(position, NEntries);
 
-        if( triggerName == "SingleMu" )
+        // Skip events that don't meet the selection criteria
+        if( dataset->triggerName == "SingleMu" )
             if( !event->fMuonFilterPassFlag ) 
                 continue;
 
+        // Get the relevant event parameters
         int pu = event->GetPEvent()->fVertex->nVtx;
         auto sums = event->GetPEvent()->fSums;
 
@@ -161,47 +93,37 @@ void makeResolutions(const int & SET, const bool & combine)
         double recoMetPhiBE = sums->caloMetPhiBE;
         double recoMetPhiHF = sums->caloMetPhi;
 
+        // Fill resolutions with event parameters. Includes the fill logic
         if( event->fMetFilterPassFlag )
         {
             if( recoMetBE > 40.0 && l1MetBE > 0.0 )
             {
-                resolution[0]->Fill(recoMetBE, l1MetBE, pu);
-                resolution[3]->Fill(FoldPhi(recoMetPhiBE), FoldPhi(l1MetPhiBE), pu);
+                if( resolutions.find("caloMetBE_l1MetBE") != resolutions.end() )
+                    resolutions["caloMetBE_l1MetBE"]->Fill(recoMetBE, l1MetBE, pu);
+                if( resolutions.find("caloMetPhiBE_l1MetPhiBE") != resolutions.end() )
+                    resolutions["caloMetPhiBE_l1MetPhiBE"]->Fill(FoldPhi(recoMetPhiBE), FoldPhi(l1MetPhiBE), pu);
             }
             if( recoMetHF > 40.0 && l1MetBE > 0.0 )
             {
-                resolution[1]->Fill(recoMetHF, l1MetBE, pu);
-                resolution[4]->Fill(FoldPhi(recoMetPhiHF), FoldPhi(l1MetPhiBE), pu);
+                if( resolutions.find("caloMetHF_l1MetBE") != resolutions.end() )
+                    resolutions["caloMetHF_l1MetBE"]->Fill(recoMetHF, l1MetBE, pu);
+                if( resolutions.find("caloMetPhiHF_l1MetPhiBE") != resolutions.end() )
+                    resolutions["caloMetPhiHF_l1MetPhiBE"]->Fill(FoldPhi(recoMetPhiHF), FoldPhi(l1MetPhiBE), pu);
             }
             if( recoMetHF > 40.0 && l1MetHF > 0.0 )
             {
-                resolution[2]->Fill(recoMetHF, l1MetHF, pu);
-                resolution[5]->Fill(recoMetPhiHF, l1MetPhiHF, pu);
+                if( resolutions.find("caloMetHF_l1MetBE") != resolutions.end() )
+                    resolutions["caloMetHF_l1MetHF"]->Fill(recoMetHF, l1MetHF, pu);
+                if( resolutions.find("caloMetPhiHF_l1MetPhiBE") != resolutions.end() )
+                    resolutions["caloMetPhiHF_l1MetPhiHF"]->Fill(FoldPhi(recoMetPhiHF), FoldPhi(l1MetPhiHF), pu);
             }
         }
     }
 
-    for(auto it=resolution.begin(); it!=resolution.end(); ++it)
-        (*it)->DrawPlots();
-        
-}
-
-std::vector<double> bins(std::string plotType)
-{
-    std::vector<double> temp;
-    if( plotType == "Energy" ) for(double binLowerEdge=-1.0; binLowerEdge<=1.5; binLowerEdge+= 0.05) temp.push_back(binLowerEdge);
-    else if( plotType == "Position" ) for(double binLowerEdge=-0.3; binLowerEdge<=0.3; binLowerEdge+= 0.005) temp.push_back(binLowerEdge);
-    return temp;
-}
-
-void SetMyStyle(int palette, double rmarg, TStyle * myStyle)
-{
-    myStyle->SetCanvasDefW(800);
-    myStyle->SetCanvasDefH(600);
-    myStyle->SetNumberContours(255);
-    myStyle->SetPalette(palette);
-    myStyle->SetPadRightMargin(rmarg);
-    myStyle->cd();
+    // End
+    for(auto it=resolutions.begin(); it!=resolutions.end(); ++it)
+        it->second->DrawPlots();
+    cout << "Output saved in:\n\t" << outDir << endl;
 }
 
 double FoldPhi(double phi)
--- submitResolutions.sh
+++ submitResolutions.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # run with:
-# $ bsub -q 8nh "sh submitResolutions.sh"
+# $ bsub -q 8nh "sh submitTurnons.sh"
 
 cd /afs/cern.ch/work/s/sbreeze/CMSSW_8_0_9/src/
 eval `scramv1 runtime -sh`
-cmsenv
 cd /afs/cern.ch/work/s/sbreeze/l1tClasses/
 
-root -q -b -l "MakePlots/makeResolutions.cxx($1,false)"
+root -q -b -l "MakePlots/makeResolutions.cxx($1,$2,$3,$4)"
