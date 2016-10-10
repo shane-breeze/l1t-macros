4408a4ef72e638dcc54fa8721fa81d07057f3a50
Shane Breeze <s.breeze15@imperial.ac.uk>
2016-10-10 10:19:49 +0200

    Need to set L1T before CERN batch submission

--- submitJetResolutions.sh
+++ submitJetResolutions.sh
@@ -2,6 +2,12 @@
 # run with:
 # $ bsub -q 8nh "sh submitTurnons.sh"
 
+if [ -z ${L1T} ]; then
+    echo "\$L1T is unset";
+else
+    cd $L1T;
+fi
+
 cd $CMSSW_BASE/src
 eval `scramv1 runtime -sh`
 cd -
